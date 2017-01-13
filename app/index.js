0ccf85466f93fcbb9730e8ae6fe07c48aef04469
AllenStu <arthurallenbalang@gmail.com>
2017-01-13 14:14:37 +0800

    material design lite implementations

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 var angular = require('angular');
 require('angular-route');
+require('material-design-lite/material.min.css');
+require('material-design-lite/material.min.js');
+require('./searchMovie/searchMovieTemplate.scss');
 
 var myModule = angular.module('myApp', [
   "ngRoute",
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,15 @@ module.exports = {
           test: /\.html$/,
           loader: 'raw',
           exclude: [/node_modules/]
+        },
+        {
+          test: /\.scss$/,
+          loader: 'style!css!sass',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          loader: 'style!css'
         }
       ]
     }
