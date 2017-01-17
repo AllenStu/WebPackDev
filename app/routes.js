9f81e7199c2d6aa2fcd69d69a6e84ad4bfa9ae95
AllenStu <arthurallenbalang@gmail.com>
2017-01-17 18:50:17 +0800

    style update

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,4 @@
 module.exports = function(ngModule) {
-<<<<<<< HEAD
   ngModule.config(routes);
 }
 
@@ -12,17 +11,3 @@ function routes($routeProvider) {
       template: "<search-movie></search-movie>"
     });
 }
-=======
-    ngModule.config(routes);
-}
-
-function routes($routeProvider) {
-    $routeProvider
-        .when("/details/:movieId", {
-            template: "<show-movie></show-movie>"
-        })
-        .otherwise({
-            template: "<search-movie></search-movie>"
-        });
-}
->>>>>>> 7e43a2ff4f56d6fbe433d511c664a66295814dc8
